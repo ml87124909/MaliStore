3be3874dda9ac0bd46705175d37fe3292eca1e27
mn3711698 <173782910@qq.com>
2019-12-30 21:51:35 +0800

    更换域名

--- config.js
+++ config.js
@@ -3,11 +3,12 @@
 var config = {
   'version': '1.5.1',
   //版本标识，不用管
-  'suburl': 'https://api.maliapi.com/api/1?viewid=home',
-  //'suburl': 'https://api.maliapi.com/api/549?viewid=home',
+  //'suburl': 'https://store.yjyzj.cn/api/1?viewid=home',
+  'suburl': 'https://store.yjyzj.cn/api/1?viewid=home',
+  //'suburl': 'https://store.yjyzj.cn/api/549?viewid=home',
   //'suburl': 'http://127.0.0.1:5001/api/1?viewid=home',
   //suburl在码力小店后台首页——>基本信息
-  'mapkey': 'DNTBZ-JZRC4-AB7US-XWO2R-KUVSZ-HVBQG',
+  'mapkey': 'TJYBZ-ES2KJ-FWEFE-KKHLP-MKHEQ-JOBYB',
   //mapkey请自己到腾讯地图开放平台申请：https://lbs.qq.com/
   'stypes': 'A',
   //stypes参数为A时：下单返现不会删除推荐人信息（用户二次购买也会返给推荐人）
